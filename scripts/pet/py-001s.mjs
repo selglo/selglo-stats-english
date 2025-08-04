41560f2aae7f93af121a4dd54f988f46a7a0dd0c
selglo <selglo2000@gmail.com>
2025-08-04 16:09:36 +0300

    Update py-001s.mjs

--- scripts/pet/py-001s.mjs
+++ scripts/pet/py-001s.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
-const INPUT_PATH = path.join('daily', 'baby', 'pettoys', 'py-001.png');
-const OUTPUT_DIR = path.join('daily', 'baby', 'pettoys', 'sliced');
+const INPUT_PATH = path.join('daily', 'pet', 'pettoys', 'py-001.png');
+const OUTPUT_DIR = path.join('daily', 'pet', 'pettoys', 'sliced');
 
 const startX = 0;
 const startY = 30;
