ccc982a0bbee802cfe45f87b1da2b8419f4b7575
selglo <selglo2000@gmail.com>
2025-08-06 14:32:24 +0300

    Update and rename pg-001s.mjs to pg-001s.mjs

--- scripts/book/pg-001s.mjs
+++ scripts/book/pg-001s.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
-const INPUT_PATH = path.join('daily', 'books', 'painting', 'pg-001.png');
-const OUTPUT_DIR = path.join('daily', 'books', 'painting', 'sliced');
+const INPUT_PATH = path.join('daily', 'book', 'painting', 'pg-001.png');
+const OUTPUT_DIR = path.join('daily', 'book', 'painting', 'sliced');
 
 const startX = 0;
 const startY = 30;
