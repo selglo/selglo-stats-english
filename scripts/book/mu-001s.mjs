e8536cefefcff56f46b037bd6b97b42d9c49e2be
selglo <selglo2000@gmail.com>
2025-08-06 14:31:41 +0300

    Update and rename mu-001s.mjs to mu-001s.mjs

--- scripts/book/mu-001s.mjs
+++ scripts/book/mu-001s.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
-const INPUT_PATH = path.join('daily', 'books', 'musical', 'mu-001.png');
-const OUTPUT_DIR = path.join('daily', 'books', 'musical', 'sliced');
+const INPUT_PATH = path.join('daily', 'book', 'musical', 'mu-001.png');
+const OUTPUT_DIR = path.join('daily', 'book', 'musical', 'sliced');
 
 const startX = 0;
 const startY = 30;
