d820a33e74630335dc6a738720568cbb00476a77
selglo <selglo2000@gmail.com>
2025-08-06 14:30:07 +0300

    Update boo-001s.mjs

--- scripts/book/boo-001s.mjs
+++ scripts/book/boo-001s.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
-const INPUT_PATH = path.join('daily', 'books', 'bookother', 'boo-001.png');
-const OUTPUT_DIR = path.join('daily', 'books', 'bookother', 'sliced');
+const INPUT_PATH = path.join('daily', 'book', 'bookother', 'boo-001.png');
+const OUTPUT_DIR = path.join('daily', 'book', 'bookother', 'sliced');
 
 const startX = 0;
 const startY = 30;
