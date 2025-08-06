a03e52b13e18b37dc12b546359c757b951083450
selglo <selglo2000@gmail.com>
2025-08-06 14:31:02 +0300

    Update and rename dr-001s.mjs to dr-001s.mjs

--- scripts/book/dr-001s.mjs
+++ scripts/book/dr-001s.mjs
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { createCanvas, loadImage } from 'canvas';
 
-const INPUT_PATH = path.join('daily', 'books', 'drawing', 'dr-001.png');
-const OUTPUT_DIR = path.join('daily', 'books', 'drawing', 'sliced');
+const INPUT_PATH = path.join('daily', 'book', 'drawing', 'dr-001.png');
+const OUTPUT_DIR = path.join('daily', 'book', 'drawing', 'sliced');
 
 const startX = 0;
 const startY = 30;
