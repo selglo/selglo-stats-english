a4e5175515ed5f13b9cd46fcb52a3b262d270275
selglo <selglo2000@gmail.com>
2025-08-07 21:53:56 +0300

    Update wo-001s.mjs

--- scripts/clothing/wo-001s.mjs
+++ scripts/clothing/wo-001s.mjs
@@ -8,7 +8,7 @@ const OUTPUT_DIR = path.join('daily', 'clothing', 'women', 'sliced');
 const startX = 0;
 const startY = 30;
 const cropWidth = 400;
-const cropHeight = 203.2;
+const cropHeight = 203.1;
 const itemCount = 100;
 
 if (!fs.existsSync(OUTPUT_DIR)) {
